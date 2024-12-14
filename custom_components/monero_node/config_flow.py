fa00adb9ae37db36558245f936e44f8c21cbc0a3
Erwin <erwin@mempyre.com>
2024-12-14 20:47:13 +0100

    1

--- custom_components/monero_node/config_flow.py
+++ custom_components/monero_node/config_flow.py
@@ -1,5 +1,6 @@
+import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN
+from .const import DOMAIN, DEFAULT_LOCAL_API, DEFAULT_GLOBAL_API, DEFAULT_COIN_API
 
 class MoneroNodeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Monero Node."""
@@ -8,20 +9,23 @@ class MoneroNodeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+        errors = {}
+
         if user_input is not None:
+            # Valider les données saisies par l'utilisateur
             return self.async_create_entry(title="Monero Node", data=user_input)
+
+        # Définir le schéma de formulaire
+        data_schema = vol.Schema(
+            {
+                vol.Optional("local_api", default=DEFAULT_LOCAL_API): str,
+                vol.Optional("global_api", default=DEFAULT_GLOBAL_API): str,
+                vol.Optional("coin_api", default=DEFAULT_COIN_API): str,
+            }
+        )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=self._get_user_schema()
+            data_schema=data_schema,
+            errors=errors,
         )
-
-    def _get_user_schema(self):
-        """Return the input schema for the user."""
-        from homeassistant.helpers.selector import TextSelector, TextSelectorConfig
-
-        return {
-            "local_api": TextSelector(TextSelectorConfig(type="url")),
-            "global_api": TextSelector(TextSelectorConfig(type="url")),
-            "coin_api": TextSelector(TextSelectorConfig(type="url")),
-        }
