cdd5114a98b723a2cd26f8c72baaf7f0cb0b8b95
Erwin <erwin@mempyre.com>
2024-12-14 20:50:51 +0100

    1

--- custom_components/monero_node/__init__.py
+++ custom_components/monero_node/__init__.py
@@ -1,8 +1,21 @@
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
+
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: ConfigType):
-    """Set up Monero Node integration."""
-    hass.data[DOMAIN] = {}
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Monero Node from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Charger les plateformes de capteurs (sensor)
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
