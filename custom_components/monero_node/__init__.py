9f4eba8aff77c7e7b55ccd09c678a9142c0f726b
Erwin <erwin@mempyre.com>
2024-12-14 14:21:23 +0100

    1

--- custom_components/monero_node/__init__.py
+++ custom_components/monero_node/__init__.py
@@ -9,10 +9,8 @@ async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Monero Node integration via YAML (if applicable)."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up the Monero Node integration via the UI."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
