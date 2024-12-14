df78feaacff980aad3276f63db40eacde4496e56
Erwin <erwin@mempyre.com>
2024-12-14 21:23:44 +0100

    1

--- custom_components/monero_node/const.py
+++ custom_components/monero_node/const.py
@@ -3,7 +3,7 @@ DEFAULT_NAME = "Monero Node"
 
 # URLs par défaut
 DEFAULT_LOCAL_API = "http://127.0.0.1:18081/json_rpc"
-DEFAULT_GLOBAL_API = "https://moneroblocks.info/api/get_height"
+DEFAULT_GLOBAL_API = "https://localmonero.co/blocks/api/get_stats"
 DEFAULT_COIN_API = "https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd"
 
 # Fréquence de mise à jour par défaut (en secondes)
